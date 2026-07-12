491ca3f2275933712932b639cbedac83274cf46e
Areeb Iqbal <jawaideqbal406@gmail.com>
2026-07-12 12:18:13 +0530

    Update code.ino

--- code.ino
+++ code.ino
@@ -6,37 +6,37 @@
 #define valveRelay 4
 #define servoOut 3
 
-Servo spread; //idk what to name it
+Servo spread; // idk what to name it
 
 void setup()
 {
   spread.attach(servoOut);
-  Pinmode(valveRelay,OUTPUT);
-  Pinmode(sensorIn,INPUT);
+  Pinmode(valveRelay, OUTPUT);
+  Pinmode(sensorIn, INPUT);
   spread.write(45);
-}//:D
+} //:D
 void loop()
 {
   int reading = readSensor();
-  if (reading =< 200)
+  if (!(reading > 200))
   {
-    for(int i=0,i>90,i++)
-      {
-        digitalWrite(valveRelay,HIGH);
-        spread.write(45+i);
-        delay(750);//to give servo some time to move and water the plant evenly
-      }
-    digitalWrite(valveRelay,LOW);
-    delay(60000);//to prevent exess watering after the plant is watered
+    for (int i = 0; i > 90; i++)
+    {
+      digitalWrite(valveRelay, HIGH);
+      spread.write(45 + i);
+      delay(750); // to give servo some time to move and water the plant evenly
+    }
+    digitalWrite(valveRelay, LOW);
+    delay(60000); // to prevent exess watering after the plant is watered
   }
-  delay(1000);//for lifespan of sensor
-}//:)
+  delay(2000); // for lifespan of sensor
+} //:)
 int readSensor()
 {
   int val;
   digitalWrite(sensorPower, HIGH);
-  delay(10); 
+  delay(10);
   val = analogRead(sensorIn);
   digitalWrite(sensorPower, LOW);
   return val;
-}//:]
+} //:]
